4895778a9e9f939fe7d6d79c8e0d39a95aef0a73
truonganhdung810 <truonganhdung810@gmail.com>
2025-05-15 01:27:54 +0700

    Drag vi tri Cover

--- src/components/CoverPhoto.jsx
+++ src/components/CoverPhoto.jsx
@@ -1,37 +1,120 @@
-import './CoverPhoto.css'
-import { useState, useRef } from 'react'
-import ImageUploadPopup from './MenuChangeCover'
-import { FaCamera } from 'react-icons/fa'
-import CameraButton from './TestComponent'
-import MenuChangeCover from './MenuChangeCover'
-import ParentComponent from './TestComponent'
+import "./CoverPhoto.css";
+import { useState, useRef, useEffect } from "react";
+import ImageUploadPopup from "./MenuChangeCover";
+import { FaCamera } from "react-icons/fa";
+import CameraButton from "./TestComponent";
+import MenuChangeCover from "./MenuChangeCover";
+import ParentComponent from "./TestComponent";
+
 function CoverPhoto() {
   const defaultUrl =
-    'https://plus.unsplash.com/premium_photo-1664635402104-75d7480fbeff'
-  const [img, setImg] = useState(defaultUrl)
-  const [originImg, setOriginImg] = useState(null)
-  const [editCoverMenuOpen, setEditCoverMenuOpen] = useState(false)
-  const [isPreviewCover, setIsPreviewCover] = useState(false)
+    "https://plus.unsplash.com/premium_photo-1664635402104-75d7480fbeff";
 
-  const buttonRef = useRef(null)
+  const [img, setImg] = useState(defaultUrl);
+  const [originImg, setOriginImg] = useState(null);
+  const [editCoverMenuOpen, setEditCoverMenuOpen] = useState(false);
+  const [isPreviewCover, setIsPreviewCover] = useState(false);
+  const [error, setError] = useState(null);
+  const buttonRef = useRef(null);
+  const coverContainerRef = useRef(null);
+  const [fullWidth, setFullWidth] = useState(1280);
+
+  // OffsetY là vị trí hiển thị của ảnh Cover so với container, đây là 1 trường cần lưu trữ kèm theo ảnh để hiển thị ảnh cho đẹp
+  const [offsetY, setOffsetY] = useState(-200);
+  const [startY, setStartY] = useState(null);
+  const [dragging, setDragging] = useState(false);
+
+  const [containerHeight, setContainerHeight] = useState(400);
+  const [imageHeight, setImageHeight] = useState(800);
+
+  // Lấy ra chiều cao (Height) của image Cover
+  useEffect(() => {
+    const imgCom = new Image();
+    imgCom.src = img;
+    imgCom.onload = () => {
+      console.log("Kich thuoc ảnh: " + imgCom.width + " x " + imgCom.height);
+      // scale ảnh to lên hoặc nhỏ lại cho bằng width của container
+      const imgH = (imgCom.height * fullWidth) / imgCom.width;
+      const imgW = (imgCom.width * fullWidth) / imgCom.width;
+      console.log("Kich thuoc ảnh scale: " + imgW + " x " + imgH);
+      setImageHeight(imgH);
+    };
+    imgCom.onerror = () => setError("Failed to load image");
+  }, [img]);
+
+  // Lấy ra width của cover-container, nếu lớn hơn ảnh, thì phải scale ảnh lên cho bằng width của container
+  useEffect(() => {
+    if (coverContainerRef.current) {
+      const w = coverContainerRef.current.offsetWidth; // Lấy chiều rộng container
+      console.log("chiều rộng container: " + w);
+      setFullWidth(w);
+    }
+
+    // Optional: Cập nhật width khi cửa sổ thay đổi kích thước
+    const handleResize = () => {
+      if (coverContainerRef.current) {
+        //console.log(coverContainerRef.current.offsetWidth);
+        //setFullWidth(coverContainerRef.current.offsetWidth);
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  // Các giá trị để thực hiện kéo thả vị trí cho ảnh Cover
+  // chỉ thực hiện khi đang preview ảnh Cover, isPreviewCover=true
+
+  const handleMouseDown = (e) => {
+    if (isPreviewCover) {
+      setStartY(e.clientY);
+      setDragging(true);
+    }
+  };
+
+  const handleMouseMove = (e) => {
+    if (!dragging) return;
+    if (isPreviewCover) {
+      const dy = e.clientY - startY;
+      setStartY(e.clientY);
+
+      // Giới hạn kéo không quá mức ảnh
+      setOffsetY((prev) => {
+        const newOffset = prev + dy;
+        const maxOffset = 0;
+        const minOffset = containerHeight - imageHeight;
+        return Math.max(Math.min(newOffset, maxOffset), minOffset);
+      });
+    }
+  };
+
+  const handleMouseUp = () => {
+    if (isPreviewCover) setDragging(false);
+  };
+  // Kêt thúc các sự kiện cho việc kéo thả vị trí ảnh Cover
 
   const setCoverImage = (urlImG) => {
-    setIsPreviewCover(true)
-    setOriginImg(img)
-    setImg(urlImG)
-  }
+    // Khởi tạo lại OffsetY=-200 là giá trị mặc định, nếu có dữ liệu OffsetY theo ảnh thì cài đặt giá trị này
+    // Ở đây chương trình không lưu lại giá trị OffsetY của ảnh nên dùng giá trị mặc định khi thay 1 ảnh cover mới
+    // Nếu chạy trên mobile, giá trị OffsetY này phải là giá trị khác
+    setOffsetY(-200);
+    setIsPreviewCover(true);
+    setOriginImg(img);
+    setImg(urlImG);
+  };
 
   const editCoverClick = () => {
-    console.log('bật popup')
-    setEditCoverMenuOpen(!editCoverMenuOpen)
-  }
+    console.log("bật popup");
+    setEditCoverMenuOpen(!editCoverMenuOpen);
+  };
 
   function cancelEdit() {
-    setImg(originImg)
-    setIsPreviewCover(false)
+    console.log(originImg);
+    setImg(originImg);
+    setIsPreviewCover(false);
   }
   function saveEdit() {
-    setIsPreviewCover(false)
+    setIsPreviewCover(false);
   }
 
   return (
@@ -47,7 +130,24 @@ function CoverPhoto() {
             </button>
           </div>
         )}
-        <img className="cover-image" src={img}></img>
+        <div
+          ref={coverContainerRef}
+          className={`img-cover-container ${dragging ? "dragging" : ""}`}
+          onMouseDown={handleMouseDown}
+          onMouseMove={handleMouseMove}
+          onMouseUp={handleMouseUp}
+          onMouseLeave={handleMouseUp}
+        >
+          <img
+            className="cover-image"
+            src={img}
+            style={{
+              height: { imageHeight },
+              transform: `translateY(${offsetY}px)`,
+              transition: dragging ? "none" : "transform 0.2s",
+            }}
+          ></img>
+        </div>
 
         {!isPreviewCover && (
           <div className="popup-container">
@@ -73,8 +173,8 @@ function CoverPhoto() {
         )}
       </div>
     </div>
-  )
+  );
 }
 
 function EditCoverPopup() {}
-export default CoverPhoto
+export default CoverPhoto;
--- src/components/MenuChangeCover.jsx
+++ src/components/MenuChangeCover.jsx
@@ -1,56 +1,65 @@
-import React from 'react'
-import { useState, useEffect, useRef } from 'react'
-import './MenuChangeCover.css'
-import { GrGallery } from 'react-icons/gr'
-import { BsUpload } from 'react-icons/bs'
-import { IoMdLink } from 'react-icons/io'
+import React from "react";
+import { useState, useEffect, useRef } from "react";
+import "./MenuChangeCover.css";
+import { GrGallery } from "react-icons/gr";
+import { BsUpload } from "react-icons/bs";
+import { IoMdLink } from "react-icons/io";
 
 const MenuChangeCover = ({ isOpen, setIsOpen, buttonRef, setCoverUrl }) => {
-  const componentRef = useRef(null)
-  const fileInputRef = useRef(null)
-  const [selectUrl, setSelectUrl] = useState(false)
-  const [imgUrl, setImgUrl] = useState(null)
-  const [errorUrl, setErrorUrl] = useState('')
+  const componentRef = useRef(null);
+  const fileInputRef = useRef(null);
+  const [selectUrl, setSelectUrl] = useState(false);
+  const [imgUrl, setImgUrl] = useState("");
+  const [isValid, setIsValid] = useState(true); // null | true | false
+  const [submitedUrl, setSubmitedUrl] = useState(false);
 
+  // Nhận file từ local
   const handleFileChange = (event) => {
-    console.log(event)
-    const file = event.target.files[0]
+    console.log(event);
+    const file = event.target.files[0];
     if (file) {
-      if (file && file.type.startsWith('image/')) {
-        const imageUrl = URL.createObjectURL(file)
-        console.log(imageUrl)
-        setCoverUrl(imageUrl) // Thay đổi cover url
-        setIsOpen(false)
+      if (file && file.type.startsWith("image/")) {
+        const imageUrl = URL.createObjectURL(file);
+        console.log(imageUrl);
+        setCoverUrl(imageUrl); // Thay đổi cover url
+        setIsOpen(false);
       }
     }
-  }
+  };
 
   const onSelect = (select) => {
-    console.log(select)
-    if (select === 'upload') {
-      fileInputRef.current.click()
-    } else if (select === 'link') {
-      setSelectUrl(true)
+    console.log(select);
+    if (select === "upload") {
+      fileInputRef.current.click();
+    } else if (select === "link") {
+      setSelectUrl(true);
     }
-  }
+  };
 
+  // Lấy link ảnh từ input url, cập nhật vào imgUrl để thay đổi Cover
   const submitUrl = (e) => {
-    e.preventDefault()
-    const imageExtensions = /\.(jpg|jpeg|png|gif)$/i
-    if (imgUrl.match(imageExtensions)) {
-      console.log('Valid Image URL:', imgUrl)
-      setErrorUrl('')
-      setIsOpen(false) // Đóng popup nếu hợp lệ
-    } else {
-      setErrorUrl(
-        'Please enter a valid image URL (e.g., .jpg, .png, .jpeg, .gif)'
-      )
-    }
-  }
+    e.preventDefault();
+    console.log("Image URL:", imgUrl);
+    console.log(submitedUrl);
+    setSubmitedUrl(true);
+  };
 
-  // Hàm xử lý click ra ngoài
+  // Load ảnh từ link
+  const handleLoad = () => {
+    setCoverUrl(imgUrl); // Thay đổi cover url
+    setIsOpen(false); // Đóng popup nếu hợp lệ
+  };
+
+  // Nếu load ảnh không hợp lệ
+  const handleError = () => {
+    console.log("error");
+    setSubmitedUrl(false);
+    setIsValid(false);
+  };
+
+  // Hàm xử lý click ra ngoài component (ở đây là popup hiển thị Menu)
   useEffect(() => {
-    console.log('use Effect: ' + isOpen)
+    console.log("use Effect: " + isOpen);
     const handleClickOutside = (event) => {
       if (
         componentRef.current &&
@@ -58,29 +67,29 @@ const MenuChangeCover = ({ isOpen, setIsOpen, buttonRef, setCoverUrl }) => {
         buttonRef.current &&
         !buttonRef.current.contains(event.target)
       ) {
-        setIsOpen(false)
+        setIsOpen(false);
       }
-    }
+    };
 
     // Gắn sự kiện click
-    document.addEventListener('click', handleClickOutside)
+    document.addEventListener("click", handleClickOutside);
 
     // Dọn dẹp sự kiện khi component unmount
     return () => {
-      document.removeEventListener('click', handleClickOutside)
-    }
-  }, [isOpen])
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, [isOpen]);
 
-  if (!isOpen) return null
+  if (!isOpen) return null;
 
   if (!selectUrl)
     return (
       <div className="popup-menu" ref={componentRef}>
-        <div className="popup-menu-item" onClick={() => onSelect('gallery')}>
+        <div className="popup-menu-item" onClick={() => onSelect("gallery")}>
           <GrGallery className="popup-menu-icon"></GrGallery>
           From Gallery
         </div>
-        <div className="popup-menu-item" onClick={() => onSelect('upload')}>
+        <div className="popup-menu-item" onClick={() => onSelect("upload")}>
           <BsUpload className="popup-menu-icon"></BsUpload>
           Upload Photo
           <input
@@ -88,20 +97,19 @@ const MenuChangeCover = ({ isOpen, setIsOpen, buttonRef, setCoverUrl }) => {
             accept="image/*"
             ref={fileInputRef}
             onChange={handleFileChange}
-            style={{ display: 'none' }} // ẩn input
+            style={{ display: "none" }} // ẩn input
           />
         </div>
-        <div className="popup-menu-item" onClick={() => onSelect('link')}>
+        <div className="popup-menu-item" onClick={() => onSelect("link")}>
           <IoMdLink className="popup-menu-icon"></IoMdLink>
           From URL
         </div>
       </div>
-    )
+    );
   else
     return (
       <div className="popup-overlay-url">
         <div className="popup-content-url">
-          <h2>Enter Image URL</h2>
           <form onSubmit={submitUrl}>
             <input
               type="text"
@@ -109,18 +117,32 @@ const MenuChangeCover = ({ isOpen, setIsOpen, buttonRef, setCoverUrl }) => {
               onChange={(e) => setImgUrl(e.target.value)}
               placeholder="Enter image URL"
               className="url-input"
+              required
             />
-            <button type="submit" className="submit-btn">
-              Submit
-            </button>
+            <div className="btn-group">
+              <button type="submit" className="submit-btn">
+                Submit
+              </button>
+              <button className="close-btn" onClick={() => setSelectUrl(false)}>
+                Close
+              </button>
+            </div>
           </form>
-          {errorUrl && <p style={{ color: 'red' }}>{errorUrl}</p>}
-          <button className="close-btn" onClick={() => setSelectUrl(false)}>
-            Close
-          </button>
+          {/* Sử dụng 1 thẻ img ẩn để load ảnh từ url, nếu có lỗi thì handle */}
+          {/* Có thể sử dụng 1 cách khác là dùng UseEfect và Image Component của React */}
+          {submitedUrl && (
+            <img
+              src={imgUrl}
+              onLoad={handleLoad}
+              onError={handleError}
+              alt="Test"
+              style={{ display: "none" }}
+            />
+          )}
+          {isValid === false && <p>❌ URL invalid</p>}
         </div>
       </div>
-    )
-}
+    );
+};
 
-export default MenuChangeCover
+export default MenuChangeCover;
